2427b7c933aabb87bf80201e2c31878c42bfc977
bang-dev <bangvo@vn.5sense.co.jp>
2024-05-06 17:24:58 +0700

    Update

--- src/app/Services/CloudinaryUploadHandler.php
+++ src/app/Services/CloudinaryUploadHandler.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services;
 
 use App\Models\Media;
@@ -6,61 +7,133 @@ use App\Models\UploadedFile as ObjUploadedFile;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
-
+use Illuminate\Support\Str;
 class CloudinaryUploadHandler implements UploadHandlerInterface
 {
     public function upload($request)
     {
         $uploadedFile = $request->file('file');
 
+        switch ($this->getFileType($uploadedFile)) {
+            case 'image':
+                return $this->uploadImage($uploadedFile);
+                break;
+            case 'video':
+                return $this->uploadVideo($uploadedFile);
+                break;
+            default:
+                return $this->uploadOtherFile($uploadedFile);
+        }
+    }
+
+    private function getFileType(UploadedFile $uploadedFile)
+    {
+        if ($this->isImage($uploadedFile)) {
+            return 'image';
+        } elseif ($this->isVideo($uploadedFile)) {
+            return 'video';
+        } else {
+            return 'other';
+        }
+    }
+
+    private function isImage(UploadedFile $uploadedFile)
+    {
+        return Str::startsWith($uploadedFile->getMimeType(), 'image/');
+    }
+
+    private function isVideo(UploadedFile $uploadedFile)
+    {
+        return Str::startsWith($uploadedFile->getMimeType(), 'video/');
+    }
+
+    private function uploadImage(UploadedFile $uploadedFile)
+    {
         $tempFilePath = $this->storeTempFile($uploadedFile);
-
-        $cloudinaryUpload = $this->uploadToCloudinary($uploadedFile);
-
+        $cloudinaryUpload = $this->uploadToCloudinary($uploadedFile,'image');
         $this->deleteTempFile($tempFilePath);
 
         if ($cloudinaryUpload) {
             $media = $this->saveMediaInfo($cloudinaryUpload, $uploadedFile);
-            $fileUploaded = new ObjUploadedFile();
-            $fileUploaded->file_name = $cloudinaryUpload->getOriginalFileName();
-            $fileUploaded->public_id = $cloudinaryUpload->getPublicId();
-            $fileUploaded->url = $cloudinaryUpload->getSecurePath();
-            $fileUploaded->save();        
-            if ($media) {
+            $fileUploaded = $this->saveUploadedInfo($cloudinaryUpload);
+            if ($media && $fileUploaded) {
                 return true;
             }
-
         }
         return false;
     }
 
-    
+    private function uploadVideo(UploadedFile $uploadedFile)
+    {
+        $tempFilePath = $this->storeTempFile($uploadedFile);
+        $cloudinaryUpload = $this->uploadToCloudinary($uploadedFile,'video');
+        $this->deleteTempFile($tempFilePath);
+
+        if ($cloudinaryUpload) {
+            $media = $this->saveMediaInfo($cloudinaryUpload, $uploadedFile);
+            $fileUploaded = $this->saveUploadedInfo($cloudinaryUpload);
+            if ($media && $fileUploaded) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function uploadOtherFile(UploadedFile $uploadedFile)
+    {
+        $tempFilePath = $this->storeTempFile($uploadedFile);
+        $cloudinaryUpload = $this->uploadToCloudinary($uploadedFile,'other');
+        $this->deleteTempFile($tempFilePath);
+
+        if ($cloudinaryUpload) {
+            $fileUploaded = $this->saveUploadedInfo($cloudinaryUpload);
+            if ($fileUploaded) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private function storeTempFile(UploadedFile $uploadedFile)
     {
         $originalFileName = $uploadedFile->getClientOriginalName();
         return $uploadedFile->storeAs('uploads', $originalFileName);
     }
-    
-    private function uploadToCloudinary(UploadedFile $uploadedFile)
+
+    private function uploadToCloudinary(UploadedFile $uploadedFile, $fileType)
     {
         $newFileName = 'file_' . time();
-        return Cloudinary::upload($uploadedFile->getRealPath(), [
+        $publicId = pathinfo($newFileName, PATHINFO_FILENAME);
+        $options = [
             'folder' => 'uploads',
-            'public_id' => pathinfo($newFileName, PATHINFO_FILENAME),
-        ]);
+            'public_id' => $publicId,
+        ];
+    
+        switch ($fileType) {
+            case 'image':
+                $options['resource_type'] = 'image';
+                break;
+            case 'video':
+                $options['resource_type'] = 'video';
+                break;
+        }
+    
+        return Cloudinary::upload($uploadedFile->getRealPath(), $options);
     }
     
+    
+
     private function deleteTempFile($tempFilePath)
     {
         unlink(storage_path('app/' . $tempFilePath));
     }
-    
+
     private function saveMediaInfo($cloudinaryUpload, UploadedFile $uploadedFile)
     {
         $fileExtension = $uploadedFile->getClientOriginalExtension();
         $fileSize = $uploadedFile->getSize();
         $mediallyType = $this->detectMediaType($fileExtension, $fileSize);
-    
+
         $media = new Media();
         $media->medially_id = rand(0, 9999999);
         $media->medially_type = $mediallyType;
@@ -69,10 +142,20 @@ class CloudinaryUploadHandler implements UploadHandlerInterface
         $media->file_type = $cloudinaryUpload->getFileType();
         $media->size = $cloudinaryUpload->getSize();
         $media->save();
-    
+
         return $media;
     }
-    
+
+    private function saveUploadedInfo($cloudinaryUpload)
+    {
+        $uploadedF = new ObjUploadedFile();
+        $uploadedF->file_name = $cloudinaryUpload->getOriginalFileName();
+        $uploadedF->public_id = $cloudinaryUpload->getPublicId();
+        $uploadedF->url = $cloudinaryUpload->getSecurePath();
+        $uploadedF->save();
+        return $uploadedF;
+    }
+
     private function detectMediaType($fileExtension, $fileSize)
     {
         $extensionToType = [
@@ -84,8 +167,7 @@ class CloudinaryUploadHandler implements UploadHandlerInterface
             'csv' => 'csv',
             'xls' => 'excel', 'xlsx' => 'excel',
         ];
-    
-      
+
         if (array_key_exists($fileExtension, $extensionToType)) {
             $mediaType = $extensionToType[$fileExtension];
             if ($mediaType === 'video' && $fileSize > 100 * 1024 * 1024) {
@@ -94,16 +176,12 @@ class CloudinaryUploadHandler implements UploadHandlerInterface
         } else {
             $mediaType = 'other';
         }
-    
+
         return $mediaType;
     }
-    
 
     public function delete($publicId)
     {
         return Cloudinary::destroy($publicId);
     }
 }
-
-
-  
